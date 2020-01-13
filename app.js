0e8e23cbdaffeb23070025207a241bc43dbd80b9
Michal Ardziejewski <adjweb@gmail.com>
2020-01-13 23:51:08 +0100

    Add play.js package

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const loudness = require('loudness')
 
-loudness.setVolume(45, (err) => {
-  console.log('Volume set: 45');
+loudness.setVolume(15, (err) => {
+  console.log('Volume set: 15');
 })
 
 loudness.getVolume((err, vol) => {
   // vol = 45
   console.log(vol);
 })
+
+
+var play = require('play');
+
+// play with a callback
+play.sound('./audio/0_min.mp3', function () {
+  console.log('play');
+  loudness.setVolume(55, (err) => {
+    console.log('Volume set: 55');
+  })
+
+});
